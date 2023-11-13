e9e4670102814429313cb4a26f3e414f3851f613
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2023-11-13 16:25:01 +0100

    added order form + message

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ function toggleTheme() {
     }
     // document.body.classList.toggle('dark-theme'); /* Toggle kollar om vår body-tagg har dark-theme som class, om den har det tar den bort det, och vice versa. Den togglar/växlar mellan tillstånden add och remove.*/
 }
+
+const orderBtn = document.querySelector('#orderBtn');
+
+orderBtn.addEventListener('click', showConfirmationDiv);
+
+function showConfirmationDiv() {
+    // 1. Dölj beställningsformuläret (orderForm)
+    document.querySelector('#orderForm').classList.add('hidden');
+
+    // Hämta namnet
+    const clientName = document.querySelector('#firstName').value;
+    document.querySelector('#name').innerHTML = clientName;
+
+    // 2. Visa beställningsbekräftelsen (orderConfirmation)
+    document.querySelector('#orderConfirmation').classList.remove('hidden');
+}
